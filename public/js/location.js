472ef6fc1e71f78cbfbfefbdef41baf04dff9329
Kamil Oczkowski <kamil@oczkow.ski>
2019-01-21 23:48:43 +0000

    Minor corrections
    Package script correction. Bus stop search input delay changed.

--- public/js/location.js
+++ public/js/location.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //Automatically getting the users location
-    $("#getLocation").click(function(){
+    $("#getLocation").click(function () {
         var startPos;
-        geoSuccess = function(position) {
+        geoSuccess = function (position) {
             // Define user position
             startPos = position.coords;
             //Ask for stops nearby and display
             getNearbyBusStops(startPos.latitude, startPos.longitude);
         };
         //Error handling
-        var geoError = function(error) {
-            switch(error.code) {
-            case error.TIMEOUT:
-                // The user didn't accept the callout
-                console.log("Could not determine location automatically. Please search for a locatin using the search box.")
-                break;
+        var geoError = function (error) {
+            switch (error.code) {
+                case error.TIMEOUT:
+                    // The user didn't accept the callout
+                    console.log("Could not determine location automatically. Please search for a locatin using the search box.")
+                    break;
             }
         };
         navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
@@ -22,12 +22,12 @@ $(document).ready(function(){
 
     //Search input listener with a delay
     var typingTimeout;
-    $('#searchInput').keyup(function() { //Start timout
+    $('#searchInput').keyup(function () { //Start timout
         clearTimeout(typingTimeout);
-        typingTimeout = setTimeout(() => getBusStops(this.value), 1500);
+        typingTimeout = setTimeout(() => getBusStops(this.value), 500);
     });
-    
-    $('#searchInput').keydown(function() {
+
+    $('#searchInput').keydown(function () {
         clearTimeout(typingTimeout);
     });
 });//End of document
