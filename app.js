1cda29e70666d3550faac4992a3f5f560d181206
Kamil Oczkowski <kamil@oczkow.ski>
2019-01-22 13:20:42 +0000

    Bus route prefix add

--- app.js
+++ app.js
@@ -2,31 +2,31 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser");
-    
+
 //APP
-    app.set("view engine", "ejs");
-    app.use(bodyParser.urlencoded({ extended : true }));
-    //Semantic-ui
-    app.use(express.static(__dirname + "/semantic/dist"));
-    //Public assets
-    app.use(express.static(__dirname + "/public"));
-    //Global middleware
-    app.use(function(req, res, next){
-        res.locals.errorCallback = function(err){
-            console.log("ERROR: " + err); //TO BE REFACTORED
-        };
-        return next(); //It's a must
-    });
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+//Semantic-ui
+app.use(express.static(__dirname + "/semantic/dist"));
+//Public assets
+app.use(express.static(__dirname + "/public"));
+//Global middleware
+app.use(function (req, res, next) {
+    res.locals.errorCallback = function (err) {
+        console.log("ERROR: " + err); //TO BE REFACTORED
+    };
+    return next(); //It's a must
+});
 //Routes
-    var indexRoute = require("./routes/index"),
-        busRoute = require("./routes/bus");
-    app.use(busRoute);// "", prefix
-    app.use(indexRoute);
+var indexRoute = require("./routes/index"),
+    busRoute = require("./routes/bus");
+app.use("/bus", busRoute);// "/bus", prefix
+app.use(indexRoute);
 
 //Web server
-    var ip = process.env.IP || "0.0.0.0",
-        port = process.env.PORT || 8000;
+var ip = process.env.IP || "0.0.0.0",
+    port = process.env.PORT || 8000;
 
-    app.listen(port, ip, function(){
-        console.log("GetMeABus have started...");
-    });
+app.listen(port, ip, function () {
+    console.log("GetMeABus have started...");
+});
--- routes/bus.js
+++ routes/bus.js
@@ -6,21 +6,21 @@ var express = require("express"),
     busSearchMw = require("../middleware/bus_search");
 
 //Bus routes
-router.get("/bus", function (req, res) {
+router.get("/", function (req, res) {
     //getMeStopTimetable('490014872N');//TESTING 1 2 1 2
     res.render("bus");
 });
-router.get("/bus/stops/:lat/:lon", function (req, res) {
+router.get("/stops/:lat/:lon", function (req, res) {
     busStopMw.getNearbyStops(req.params.lat, req.params.lon).then((nearbyStops) => {
         res.json(nearbyStops);
     });
 });
-router.post("/bus/arrivals", function (req, res) {
+router.post("/arrivals", function (req, res) {
     busArrivalMw.getMeStopTimetable(req.body.naptanId).then((arrivals) => {
         res.json(arrivals);
     });
 });
-router.get("/bus/stop/:value", function (req, res) {
+router.get("/stop/:value", function (req, res) {
     busSearchMw.findStopsLocation(req.params.value).then((foundStops) => {
         res.json(foundStops);
     });
